b4dfb7d710592b9160a57ca364d70d2d90c788c2
jeremyc2 <goldenmeso@gmail.com>
2017-05-09 13:06:52 -0600

    Deque
    Changed all references to Queue to Deque

--- deque.h
+++ deque.h
@@ -1,43 +1,40 @@
 /***********************************************************************
 * Header:
-*    Queue
+*    Deque
 * Summary:
-*    This class contains the notion of a stack: a container
-*    that follows First In First Out behavior and
-*    expands as more items are put inside.
 *
 *    This will contain the class definition of:
-*        Queue         : A class that holds and uses FIFO behavior
+*        Deque         : 
 * Author
 *    Nathan Bench, Jed Billman, Justin Chandler, Jeremy Chandler
 ************************************************************************/
 
-#ifndef QUEUE_H
-#define QUEUE_H
+#ifndef DEQUE_H
+#define DEQUE_H
 
 #include <cassert>
 #include <new>
 #include <iostream>
 
 /************************************************
- * QUEUE
+ * DEQUE
  * A class that holds stuff
  ***********************************************/
 template <class T>
-class Queue
+class Deque
 {
 public:
    // default constructor : empty and kinda useless
-   Queue() : m_numPush(0), m_numPop(0), m_capacity(0), m_data(NULL) {}
+   Deque() : m_numPush(0), m_numPop(0), m_capacity(0), m_data(NULL) {}
 
    // copy constructor : copy it
-   Queue(const Queue<T> & rhs) throw (const char *);
+   Deque(const Deque<T> & rhs) throw (const char *);
    
    // non-default constructor : pre-allocate
-   Queue(int capacity) throw (const char *);
+   Deque(int capacity) throw (const char *);
    
    // destructor : free everything
-   ~Queue()        { if (!empty()) delete [] m_data; }
+   ~Deque()        { if (!empty()) delete [] m_data; }
    
    // is the container currently empty
    bool empty() const  
@@ -54,28 +51,28 @@ public:
    // how many items are currently in the container?
    int size() const    { return (m_numPush - m_numPop);              }   // cite: BYUI PDF page 83
 
-   // add an item to the back of the queue
+   // add an item to the back of the deque
    void push(const T & t) throw (const char *);
 
-   // Removes an item from the front of the queue
+   // Removes an item from the front of the deque
    void pop() throw (const char *);
 
-   // Returns the item currently at the front of the queue
+   // Returns the item currently at the front of the deque
    T & front()     throw (const char *);
    //T front() const throw (const char *);
    
-   // Returns the item currently at the front of the queue
+   // Returns the item currently at the front of the deque
    T & back()     throw (const char *);
    //T back() const throw (const char *);
    
    // assignment operator '='
-   Queue<T> & operator = (const Queue <T> & rhs);
+   Deque<T> & operator = (const Deque <T> & rhs);
    
 private:
    T * m_data;          // dynamically allocated array of T
    int m_numPush,       // cite: BYUI PDF page 82
        m_numPop,
-       m_capacity;      // how many items can I put on the Queue before full?
+       m_capacity;      // how many items can I put on the Deque before full?
    
    // private methods
    int iHead() const { return m_numPop % m_capacity; }           // cite: BYUI PDF page 82
@@ -85,10 +82,10 @@ private:
 };
 
 /*******************************************
- * QUEUE :: COPY CONSTRUCTOR
+ * DEQUE :: COPY CONSTRUCTOR
  *******************************************/
 template <class T>
-Queue <T> :: Queue(const Queue <T> & rhs) throw (const char *)
+Deque <T> :: Deque(const Deque <T> & rhs) throw (const char *)
 {
    assert(rhs.m_capacity >= 0);
 
@@ -102,7 +99,7 @@ Queue <T> :: Queue(const Queue <T> & rhs) throw (const char *)
       }
       catch (std::bad_alloc)
       {
-         throw "ERROR: Unable to allocate a new buffer for Queue";
+         throw "ERROR: Unable to allocate a new buffer for Deque";
       }
    
    // insure the indices are beginning at 0
@@ -126,11 +123,11 @@ Queue <T> :: Queue(const Queue <T> & rhs) throw (const char *)
 }
 
 /**********************************************
- * QUEUE : NON-DEFAULT CONSTRUCTOR
+ * DEQUE : NON-DEFAULT CONSTRUCTOR
  * Preallocate the container to "capacity"
  **********************************************/
 template <class T>
-Queue <T> :: Queue(int capacity) throw (const char *)
+Deque <T> :: Deque(int capacity) throw (const char *)
 {
    assert(capacity >= 0);
    
@@ -163,11 +160,11 @@ Queue <T> :: Queue(int capacity) throw (const char *)
 }
 
 /***************************************************
-* QUEUE :: PUSH
-* Adds an item to the back of the queue
+* DEQUE :: PUSH
+* Adds an item to the back of the deque
 **************************************************/
 template<class T>
-void Queue<T>::push(const T & t) throw (const char *)
+void Deque<T>::push(const T & t) throw (const char *)
 {
 	if (size() == capacity())
 	{
@@ -179,72 +176,72 @@ void Queue<T>::push(const T & t) throw (const char *)
 }
 
 /***************************************************
-* QUEUE :: POP
-* Removes an item from the front of the queue
+* DEQUE :: POP
+* Removes an item from the front of the deque
 **************************************************/
 template<class T>
-void Queue<T>::pop() throw(const char *)
+void Deque<T>::pop() throw(const char *)
 {
 	if (empty())
-		throw "ERROR: attempting to pop from an empty queue";
+		throw "ERROR: attempting to pop from an empty deque";
 	m_numPop++;
 }
 
 /***************************************************
-* QUEUE :: FRONT
-* Returns the item currently at the front of the queue by reference
+* DEQUE :: FRONT
+* Returns the item currently at the front of the deque by reference
 **************************************************/
 template<class T>
-T & Queue<T> :: front() throw(const char *)
+T & Deque<T> :: front() throw(const char *)
 {
 	// if empty
 	if (empty())
-		throw "ERROR: attempting to access an item in an empty queue";
+		throw "ERROR: attempting to access an item in an empty deque";
 	return m_data[iHead()];
 }
 /***************************************************
-* QUEUE :: FRONT C
-* Returns the item currently at the front of the queue by const value
+* DEQUE :: FRONT C
+* Returns the item currently at the front of the deque by const value
 **************************************************/
 /*template<class T>
-T Queue<T> :: front() const throw(const char *)
+T Deque<T> :: front() const throw(const char *)
 {
 	// if empty
 	if (empty())
-		throw "ERROR: attempting to access an item in an empty queue";
+		throw "ERROR: attempting to access an item in an empty deque";
 	return m_data[iHead()];      
 }*/
 
 /***************************************************
-* QUEUE :: BACK
-* Returns the item currently at the back of the queue by reference
+* DEQUE :: BACK
+* Returns the item currently at the back of the deque by reference
 **************************************************/
 template<class T>
-T & Queue<T> :: back()     throw (const char *)
+T & Deque<T> :: back()     throw (const char *)
 {
    if (empty())
-		throw "ERROR: attempting to access an item in an empty queue";
+		throw "ERROR: attempting to access an item in an empty deque";
 	return m_data[iTail()]; 
 }
    
 /***************************************************
-* QUEUE :: BACK C
-* Returns the item currently at the back of the queue by const value
+* DEQUE :: BACK C
+* Returns the item currently at the back of the deque by const value
 **************************************************/
 /*template<class T>
-T Queue<T> :: back() const throw (const char *)
+T Deque<T> :: back() const throw (const char *)
 {
    if (empty())
-		throw "ERROR: attempting to access an item in an empty queue";
+		throw "ERROR: attempting to access an item in an empty deque";
 	return m_data[iTail()]; 
 }*/
 
 /***************************************************
- * QUEUE :: =
+ * DEQUE :: =
  * Overload assignment operator
  **************************************************/
  template <class T>
- Queue<T> & Queue <T> :: operator = (const Queue <T> & rhs)
+ Deque<T> & Deque <T> :: operator = (const Deque <T> & rhs)
  {
 	 // don't copy yourself
 	 if (this != &rhs)
@@ -273,11 +270,11 @@ T Queue<T> :: back() const throw (const char *)
  }
    
 /***************************************************
-* QUEUE :: RESIZE
+* DEQUE :: RESIZE
 * Allocate memory for m_data
 **************************************************/
 template<class T>
-void Queue<T>::resize(int newCap)
+void Deque<T>::resize(int newCap)
 {
    if (newCap == 0)
       newCap = 1;
@@ -300,9 +297,9 @@ void Queue<T>::resize(int newCap)
    }
    catch(std::bad_alloc)
    {
-      throw "ERROR: Unable to allocate a new buffer for Queue";
+      throw "ERROR: Unable to allocate a new buffer for Deque";
    }
 
 }
 
-#endif // QUEUE_H
+#endif // DEQUE_H
