2725d265fd73bb91f0545f6fb51e5a997e6a5083
Justin9four <justinchandler.17@gmail.com>
2017-05-10 08:18:24 -0600

    added nowServing.cpp & nowServing.h

---
+++ nowServing.h
@@ -0,0 +1,33 @@
+/***********************************************************************
+* Header:
+*    NowServing
+* Summary:
+*
+*    This will contain the class definition of:
+*        NowServing         :
+* Author
+*    Nathan Bench, Jed Billman, Justin Chandler, Jeremy Chandler (Leonel Messi)
+************************************************************************/
+
+#ifndef NOWSERVING_H
+#define NOWSERVING_H
+
+#include <cassert>
+#include <new>
+#include <iostream>
+
+/************************************************
+* NOWSERVING
+* A class that holds stuff
+***********************************************/
+template <class T>
+class NowServing
+{
+public:
+
+private:
+
+};
+
+
+#endif // NOWSERVING_H
