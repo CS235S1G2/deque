a76f50f78236e892234a0614e6f79248d401387e
nhbench <nhbench@gmail.com>
2017-05-11 15:29:40 -0400

    Update nowServing.h
    * wrote Student class

--- nowServing.h
+++ nowServing.h
@@ -1,33 +1,59 @@
 /***********************************************************************
-* Header:
-*    NowServing
-* Summary:
-*
-*    This will contain the class definition of:
-*        NowServing         :
-* Author
-*    Nathan Bench, Jed Billman, Justin Chandler, Jeremy Chandler (Leonel Messi)
-************************************************************************/
+ * Header:
+ *    NOW SERVING
+ * Summary:
+ *    This will contain just the prototype for nowServing(). You may
+ *    want to put other class definitions here as well.
+ * Author
+ *    Nathan Bench, Jed Billman, Justin Chandler, Jeremy Chandler (Leonel Messi)
+ ************************************************************************/
 
-#ifndef NOWSERVING_H
-#define NOWSERVING_H
+#ifndef NOW_SERVING_H
+#define NOW_SERVING_H
 
-#include <cassert>
-#include <new>
-#include <iostream>
+//#include "deque.h"     // for DEQUE
+#include <deque>
+
+// the interactive nowServing program
+void nowServing();
 
 /************************************************
-* NOWSERVING
-* A class that holds stuff
-***********************************************/
-template <class T>
-class NowServing
-{
-public:
+ * STUDENT
+ * name, class, minutes
+ ***********************************************/
+ class Student
+ {
+   private:
+      std::string name;
+      std::string className;
+      int minutes;
+      bool isEmergency;
+      
+   public:
+      // default constructor
+      Student(): minutes(0) {}
+      
+      // non-default constructor
+      Student(std::string & name, std::string & className, int & minutes, bool & emergency);
+      
+      // copy constructor
+      Student(const Student & rhs): name(rhs.name), className(rhs.className), 
+      minutes(rhs.minutes), isEmergency(rhs.isEmergency) 
+         {} 
+      
+      // getters
+      std::string getName()                  { return name;                 }
+      std::string getClass()                 { return className;            }
+      int getMinutes()                       { return minutes;              }
+      bool isEmergent()                      { return isEmergency;          }
+      
+      // setters
+      void setName(std::string name)         { this->name = name;           }
+      void setClass(std::string className)   { this->className = className; }
+      void setMinutes(int minutes)           { this->minutes = minutes;     }
+      void setEmergency(bool emergency)      { isEmergency = emergency;      }
+      
+      void subMin()                          { minutes--;                   }
+ };
 
-private:
-
-};
-
-
-#endif // NOWSERVING_H
+#endif // NOW_SERVING_H
